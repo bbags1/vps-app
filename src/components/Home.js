79206102099706974cce128b4d79c63b881d8d54
Brody Bagnall <brodybagnall@Brodys-MacBook-Air.local>
2024-09-11 20:37:20 -0700

    main

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,19 @@ function Home() {
   return (
     <div className="home">
       <div className="content">
-        <h1>Welcome to Vancouver Property Services!</h1>
-        <p>We are independent, licensed contractors located in Vancouver Washington</p>
-        <p>We specialize in residential bathroom remodeling, tile work, trim, flooring, and all things required to turn your home into the comfortable space it should be.</p>
-        <p>We focus on the little details and doing things the right way.</p>
-        <p>Give us a call today to find out how we can assist you in your home service needs!</p>
+        <h1>Expert Bathroom Remodeling in Vancouver, WA</h1>
+        <p>Welcome to Vancouver Property Services, your trusted partner for high-quality bathroom renovations!</p>
+        <p>As independent, licensed contractors located in Vancouver, Washington, we specialize in:</p>
         <ul>
-          <li><Link to="/contact" className="quote-button">Contact Us</Link></li>
+          <li>Residential bathroom remodeling</li>
+          <li>Tile work</li>
+          <li>Custom trim</li>
+          <li>Flooring</li>
+          <li>Home Remodeling</li>
         </ul>
+        <p>Our focus on attention to detail and commitment to excellence ensures that your bathroom remodel will exceed your expectations.</p>
+        <p>Transform your bathroom into a comfortable, stylish space with Vancouver Property Services.</p>
+        <Link to="/contact" className="quote-button">Get a Free Quote Today!</Link>
       </div>
     </div>
   );
